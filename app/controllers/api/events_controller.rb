3e902a52a21915ac005733286931c19ec8fd8c0d
fiteclub <mattf255@yahoo.com>
2021-01-12 23:51:46 -0500

    Add event policy

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,6 +2,7 @@ module Api
   class EventsController < ApplicationController
     def index
       @events = Group.friendly.find(params[:group_id]).events.order(start_at: :desc)
+      authorize @events
     end
   end
 end
--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -1,3 +1,6 @@
 class EventPolicy < ApplicationPolicy
-  
+  def index?
+    # What roles have access to this?
+    true
+  end
 end
