09125c6f9842221d9766f6ab96cb25580b9a6232
fiteclub <mattf255@yahoo.com>
2021-06-10 18:00:44 -0400

    Fix Twitter login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_or_create_with_omniauth(auth)
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,11 @@
+OmniAuth.config.allowed_request_methods = [:post, :get]
+OmniAuth.config.silence_get_warning = true
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter,
            Rails.application.credentials.twitter[:consumer_key],
-           Rails.application.credentials.twitter[:consumer_secret]
+           Rails.application.credentials.twitter[:consumer_secret],
+           origin_param: 'return_to'
 
   OmniAuth.config.on_failure = proc { |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
